7582d2d294dc7152b92364a87d78f95d9bd8387c
Yana_Ratnikova <yardley1@yandex.ru>
2023-06-02 17:42:00 +0300

    Модуль с решениями

--- modul.py
+++ modul.py
@@ -1,17 +1,18 @@
-#Сложение элементов
+# Сложение элементов
 def sum_args(*args):
     res = 0
     for i in args:
         res += int(i)
     return res
 
-#Возведение в степень с помощью рекурсии
+# Возведение в степень с помощью рекурсии
 def exponentiation(a, b):
     if b > 0:
         return a*exponentiation(a, b - 1)
     else:
         return 1
     
+# Сумма двух чисел с помощью рекурсии    
 def sum_nums(a, b):
     if b > 0:
         return 1 + sum_nums(a, b - 1)
